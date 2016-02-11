54ee6318822d783e2706b7bcddc19cc9fa07f78d
Tiziano Santoro <tzn@google.com>
2016-02-11 20:59:23 +0000

    update expected output

--- protoc-gen-elm/test/1/expected_output/test.elm
+++ protoc-gen-elm/test/1/expected_output/test.elm
@@ -1,14 +1,40 @@
-import Json.Decode
+import Json.Decode as JD exposing ((:=))
+
+
+type Enum
+  = EnumValueDefault -- 0
+  | EnumValue1 -- 1
+  | EnumValue2 -- 2
+  | EnumValue123 -- 123
+
+
+enum : JD.Decoder Enum
+enum =
+  let
+    lookup s = case s of
+      "ENUM_VALUE_DEFAULT" -> EnumValueDefault
+      "ENUM_VALUE_1" -> EnumValue1
+      "ENUM_VALUE_2" -> EnumValue2
+      "ENUM_VALUE_123" -> EnumValue123
+      _ -> EnumValueDefault
+  in
+    JD.map lookup JD.string
+
 
 type alias Foo =
   { int64_field : Int
   , bool_field : Bool
   , string_field : String
+  , enum_field : Enum
   }
 
-foo : Decoder Foo
+
+foo : JD.Decoder Foo
 foo =
-  objectN Foo
-    (int64_field := int)
-    (bool_field := bool)
-    (string_field := string)
+  JD.object4 Foo
+    ("int64_field" := JD.int)
+    ("bool_field" := JD.bool)
+    ("string_field" := JD.string)
+    ("enum_field" := enum)
+
+
