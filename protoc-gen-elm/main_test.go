9d642d5d0b48b8240253a306b405cb7c39d347b3
Tiziano Santoro <tiziano88@gmail.com>
2016-03-08 22:42:36 +0000

    Initial tests

--- protoc-gen-elm/main_test.go
+++ protoc-gen-elm/main_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestDiff(t *testing.T) {
 	wd, _ := os.Getwd()
-	td := filepath.Join(wd, "test")
+	td := filepath.Join(wd, "Test")
 
 	dirs, err := ioutil.ReadDir(td)
 	if err != nil {
@@ -22,7 +22,7 @@ func TestDiff(t *testing.T) {
 		}
 
 		dir := filepath.Join(td, fi.Name())
-		actualOutputDir := filepath.Join(dir, "actual_output")
+		actualOutputDir := filepath.Join(dir, "Actual")
 
 		err := os.RemoveAll(actualOutputDir)
 		if err != nil {
@@ -41,8 +41,8 @@ func TestDiff(t *testing.T) {
 }
 
 func runProto(t *testing.T, dir string) {
-	cmd := exec.Command("protoc", "--elm_out=../actual_output", "test.proto")
-	cmd.Dir = filepath.Join(dir, "input")
+	cmd := exec.Command("protoc", "--elm_out=../Actual", "test.proto")
+	cmd.Dir = filepath.Join(dir, "Input")
 	t.Logf("cmd: %v", cmd)
 	out, err := cmd.CombinedOutput()
 	t.Logf("Output: %s", out)
@@ -52,7 +52,7 @@ func runProto(t *testing.T, dir string) {
 }
 
 func runDiff(t *testing.T, dir string) {
-	cmd := exec.Command("diff", "-y", "expected_output", "actual_output")
+	cmd := exec.Command("diff", "-y", "Expected", "Actual")
 	cmd.Dir = dir
 	out, err := cmd.CombinedOutput()
 	if err != nil {
