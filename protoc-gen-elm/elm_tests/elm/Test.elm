8f8c885db79abf6917d7164ac4f6c2c9957536af
Tiziano Santoro <tiziano88@gmail.com>
2016-03-12 20:03:54 +0000

    Format tests

--- protoc-gen-elm/elm_tests/elm/Test.elm
+++ protoc-gen-elm/elm_tests/elm/Test.elm
@@ -12,15 +12,32 @@ import Simple as T
 
 tests : Test
 tests =
-    suite "A Test Suite"
-        [ test "JSON encode" (assertEqual (JE.encode 2 (T.simpleEncoder msg)) msgJson)
-        , test "JSON decode" (assertEqual (JD.decodeString T.simpleDecoder msgJson) (Result.Ok msg))
-        ]
+  suite "A Test Suite"
+    [ test "JSON encode" (assertEqual (JE.encode 2 (T.simpleEncoder msg)) msgJson)
+    , test "JSON decode" <| assertDecode T.simpleDecoder msgJson msg
+    , test "JSON decode extra field" <| assertDecode T.simpleDecoder msgExtraFieldJson msg
+    , test "JSON decode empty message" <| assertDecode T.simpleDecoder msgEmptyJson msgDefault
+    ]
+
+
+assertDecode : JD.Decoder a -> String -> a -> Assertion
+assertDecode decoder json msg =
+  assertEqual
+    (Result.Ok msg)
+    (JD.decodeString decoder json)
+
 
 msg : T.Simple
 msg =
   { int32Field = 123
   }
+
+
+msgDefault : T.Simple
+msgDefault =
+  { int32Field = 0
+  }
+
 
 msgJson : String
 msgJson = String.trim """
@@ -30,6 +47,21 @@ msgJson = String.trim """
 """
 
 
+msgExtraFieldJson : String
+msgExtraFieldJson = String.trim """
+{
+  "int32Field": 123,
+  "extraField": "abc"
+}
+"""
+
+
+msgEmptyJson = String.trim """
+{
+}
+"""
+
+
 port runner : Signal (Task.Task x ())
 port runner =
-    Console.run (consoleRunner tests)
+  Console.run (consoleRunner tests)
