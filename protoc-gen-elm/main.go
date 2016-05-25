9e4b156d5eb80b82eb8aa1a24c8ac6cadcc5cf66
Tiziano Santoro <tiziano88@gmail.com>
2016-05-25 15:10:43 +0100

    Split into multiple files

--- protoc-gen-elm/main.go
+++ protoc-gen-elm/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -102,112 +101,6 @@ func processFile(inFile *descriptor.FileDescriptorProto) (*plugin.CodeGeneratorR
 	outFile.Content = proto.String(fg.out.String())
 
 	return outFile, nil
-}
-
-type FileGenerator struct {
-	out    bytes.Buffer
-	indent uint
-}
-
-func NewFileGenerator() *FileGenerator {
-	return &FileGenerator{}
-}
-
-func (fg *FileGenerator) In() {
-	fg.indent += 1
-}
-
-func (fg *FileGenerator) Out() {
-	fg.indent -= 1
-}
-
-func (fg *FileGenerator) P(format string, a ...interface{}) error {
-	var err error
-
-	_, err = fg.out.WriteString(strings.Repeat("  ", int(fg.indent)))
-	if err != nil {
-		return err
-	}
-
-	s := fmt.Sprintf(format, a...)
-	_, err = fg.out.WriteString(s)
-	if err != nil {
-		return err
-	}
-	_, err = fg.out.WriteString("\n")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (fg *FileGenerator) GenerateEnum(prefix string, inEnum *descriptor.EnumDescriptorProto) error {
-	typeName := prefix + inEnum.GetName()
-	fg.P("type %s", typeName)
-	fg.In()
-	leading := "="
-	for _, enumValue := range inEnum.GetValue() {
-		// TODO: Convert names to CamelCase.
-		fg.P("%s %s -- %d", leading, prefix+elmEnumValueName(enumValue.GetName()), enumValue.GetNumber())
-		leading = "|"
-	}
-	fg.Out()
-	return nil
-}
-
-func (fg *FileGenerator) GenerateEnumDecoder(prefix string, inEnum *descriptor.EnumDescriptorProto) error {
-	typeName := prefix + inEnum.GetName()
-	decoderName := decoderName(typeName)
-	fg.P("%s : JD.Decoder %s", decoderName, typeName)
-	fg.P("%s =", decoderName)
-	fg.In()
-	fg.P("let")
-	fg.In()
-	fg.P("lookup s = case s of")
-	fg.In()
-	for _, enumValue := range inEnum.GetValue() {
-		fg.P("%q -> %s", enumValue.GetName(), prefix+elmEnumValueName(enumValue.GetName()))
-	}
-	// TODO: This should fail instead.
-	fg.P("_ -> %s", prefix+elmEnumValueName(inEnum.GetValue()[0].GetName()))
-	fg.Out()
-	fg.Out()
-	fg.P("in")
-	fg.In()
-	fg.P("JD.map lookup JD.string")
-	fg.Out()
-	fg.Out()
-
-	fg.P("")
-	fg.P("")
-
-	defaultName := defaultEnumValue(typeName)
-	fg.P("%s : %s", defaultName, typeName)
-	fg.P("%s = %s", defaultName, prefix+elmEnumValueName(inEnum.GetValue()[0].GetName()))
-	return nil
-}
-
-func (fg *FileGenerator) GenerateEnumEncoder(prefix string, inEnum *descriptor.EnumDescriptorProto) error {
-	typeName := prefix + inEnum.GetName()
-	argName := "v"
-	fg.P("%s : %s -> JE.Value", encoderName(typeName), typeName)
-	fg.P("%s %s =", encoderName(typeName), argName)
-	fg.In()
-	fg.P("let")
-	fg.In()
-	fg.P("lookup s = case s of")
-	fg.In()
-	for _, enumValue := range inEnum.GetValue() {
-		fg.P("%s -> %q", prefix+elmEnumValueName(enumValue.GetName()), enumValue.GetName())
-	}
-	fg.Out()
-	fg.Out()
-	fg.P("in")
-	fg.In()
-	fg.P("JE.string <| lookup %s", argName)
-	fg.Out()
-	fg.Out()
-	return nil
 }
 
 func (fg *FileGenerator) GenerateModule(moduleName string) {
@@ -333,40 +226,10 @@ func (fg *FileGenerator) GenerateRuntime() {
 	fg.P("")
 }
 
-func (fg *FileGenerator) GenerateEnumThings(prefix string, inEnum *descriptor.EnumDescriptorProto) error {
-	var err error
-
-	err = fg.GenerateEnum(prefix, inEnum)
-	if err != nil {
-		return err
-	}
-
-	fg.P("")
-	fg.P("")
-
-	err = fg.GenerateEnumDecoder(prefix, inEnum)
-	if err != nil {
-		return err
-	}
-
-	fg.P("")
-	fg.P("")
-
-	err = fg.GenerateEnumEncoder(prefix, inEnum)
-	if err != nil {
-		return err
-	}
-
-	fg.P("")
-	fg.P("")
-
-	return nil
-}
-
 func (fg *FileGenerator) GenerateEverything(prefix string, inMessage *descriptor.DescriptorProto) error {
 	var err error
 
-	err = fg.GenerateMessage(prefix, inMessage)
+	err = fg.GenerateMessageDefinition(prefix, inMessage)
 	if err != nil {
 		return err
 	}
@@ -443,58 +306,6 @@ func fieldElmType(inField *descriptor.FieldDescriptorProto) string {
 	}
 }
 
-func (fg *FileGenerator) GenerateMessage(prefix string, inMessage *descriptor.DescriptorProto) error {
-	typeName := prefix + inMessage.GetName()
-	fg.P("type alias %s =", typeName)
-	fg.In()
-
-	leading := "{"
-	for _, inField := range inMessage.GetField() {
-		optional := (inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_OPTIONAL) &&
-			(inField.GetType() == descriptor.FieldDescriptorProto_TYPE_MESSAGE)
-		repeated := inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_REPEATED
-
-		fType := fieldElmType(inField)
-
-		fName := elmFieldName(inField.GetName())
-		fNumber := inField.GetNumber()
-
-		if repeated {
-			fg.P("%s %s : List %s -- %d", leading, fName, fType, fNumber)
-		} else {
-			if optional {
-				fg.P("%s %s : Maybe %s -- %d", leading, fName, fType, fNumber)
-			} else {
-				fg.P("%s %s : %s -- %d", leading, fName, fType, fNumber)
-			}
-		}
-
-		leading = ","
-	}
-
-	for _, inOneof := range inMessage.GetOneofDecl() {
-
-		oneofName := elmFieldName(inOneof.GetName())
-		// TODO: Prefix with message name to avoid collisions.
-		oneofTypeName := elmTypeName(inOneof.GetName())
-		fg.P("%s %s : %s", leading, oneofName, oneofTypeName)
-
-		leading = ","
-	}
-
-	fg.P("}")
-	fg.Out()
-
-	for i, _ := range inMessage.GetOneofDecl() {
-		fg.P("")
-		fg.GenerateOneofDefinition(prefix, inMessage, i)
-		fg.GenerateOneofDecoder(prefix, inMessage, i)
-		fg.GenerateOneofEncoder(prefix, inMessage, i)
-	}
-
-	return nil
-}
-
 func elmOneofDecoderName(inOneof *descriptor.OneofDescriptorProto) string {
 	typeName := elmTypeName(inOneof.GetName())
 	return decoderName(typeName)
@@ -502,68 +313,6 @@ func elmOneofDecoderName(inOneof *descriptor.OneofDescriptorProto) string {
 
 func elmOneofTypeName(inOneof *descriptor.OneofDescriptorProto) string {
 	return elmTypeName(inOneof.GetName())
-}
-
-func (fg *FileGenerator) GenerateOneofDefinition(prefix string, inMessage *descriptor.DescriptorProto, oneofIndex int) error {
-	inOneof := inMessage.GetOneofDecl()[oneofIndex]
-
-	// TODO: Prefix with message name to avoid collisions.
-	oneofTypeName := elmOneofTypeName(inOneof)
-	fg.P("type %s", oneofTypeName)
-
-	fg.In()
-
-	leading := "="
-	for _, inField := range inMessage.GetField() {
-		if inField.OneofIndex != nil && inField.GetOneofIndex() == int32(oneofIndex) {
-
-			oneofVariantName := elmTypeName(inField.GetName())
-			oneofArgumentType := fieldElmType(inField)
-			fg.P("%s %s %s", leading, oneofVariantName, oneofArgumentType)
-
-			leading = "|"
-		}
-	}
-	fg.Out()
-	fg.P("")
-
-	return nil
-}
-
-func (fg *FileGenerator) GenerateOneofDecoder(prefix string, inMessage *descriptor.DescriptorProto, oneofIndex int) error {
-	inOneof := inMessage.GetOneofDecl()[oneofIndex]
-
-	// TODO: Prefix with message name to avoid collisions.
-	typeName := elmOneofTypeName(inOneof)
-	decoderName := elmOneofDecoderName(inOneof)
-
-	fg.P("%s : JD.Decoder %s", decoderName, typeName)
-	fg.P("%s =", decoderName)
-
-	fg.In()
-
-	fg.P("JD.oneOf")
-	fg.In()
-
-	leading := "["
-	for _, inField := range inMessage.GetField() {
-		if inField.OneofIndex != nil && inField.GetOneofIndex() == int32(oneofIndex) {
-			oneofVariantName := elmTypeName(inField.GetName())
-			decoderName := fieldElmDecoderName(inField)
-			fg.P("%s JD.map %s (%q := %s)", leading, oneofVariantName, inField.GetJsonName(), decoderName)
-			leading = ","
-		}
-	}
-	fg.P("]")
-	fg.Out()
-	fg.Out()
-	fg.P("")
-
-	return nil
-}
-
-func (fg *FileGenerator) GenerateOneofEncoder(prefix string, inMessage *descriptor.DescriptorProto, oneofIndex int) error {
-	return nil
 }
 
 func fieldElmDefaultValue(inField *descriptor.FieldDescriptorProto) string {
@@ -631,112 +380,6 @@ func fieldElmDecoderName(inField *descriptor.FieldDescriptorProto) string {
 	default:
 		return fmt.Sprintf("Error generating decoder for field %s", inField.GetType())
 	}
-}
-
-func (fg *FileGenerator) GenerateMessageDecoder(prefix string, inMessage *descriptor.DescriptorProto) error {
-	typeName := prefix + inMessage.GetName()
-	fg.P("%s : JD.Decoder %s", decoderName(typeName), typeName)
-	fg.P("%s =", decoderName(typeName))
-	fg.In()
-	fg.P("%s", typeName)
-	fg.In()
-
-	leading := "<$>"
-	for _, inField := range inMessage.GetField() {
-		optional := (inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_OPTIONAL) &&
-			(inField.GetType() == descriptor.FieldDescriptorProto_TYPE_MESSAGE)
-		repeated := inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_REPEATED
-		d := fieldElmDecoderName(inField)
-		def := fieldElmDefaultValue(inField)
-
-		if repeated {
-			fg.P("%s (repeatedFieldDecoder %q %s)", leading, jsonFieldName(inField), d)
-		} else {
-			if optional {
-				fg.P("%s (optionalFieldDecoder %q %s)", leading, jsonFieldName(inField), d)
-			} else {
-				fg.P("%s (requiredFieldDecoder %q %s %s)", leading, jsonFieldName(inField), def, d)
-			}
-		}
-
-		leading = "<*>"
-	}
-
-	for _, inOneof := range inMessage.GetOneofDecl() {
-		oneofDecoderName := elmOneofDecoderName(inOneof)
-		fg.P("%s %s", leading, oneofDecoderName)
-
-		leading = "<*>"
-	}
-
-	fg.Out()
-	fg.Out()
-	return nil
-}
-
-func (fg *FileGenerator) GenerateMessageEncoder(prefix string, inMessage *descriptor.DescriptorProto) error {
-	typeName := prefix + inMessage.GetName()
-	argName := "v"
-	fg.P("%s : %s -> JE.Value", encoderName(typeName), typeName)
-	fg.P("%s %s =", encoderName(typeName), argName)
-	fg.In()
-	fg.P("JE.object")
-	fg.In()
-
-	leading := "["
-	for _, inField := range inMessage.GetField() {
-		optional := (inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_OPTIONAL) &&
-			(inField.GetType() == descriptor.FieldDescriptorProto_TYPE_MESSAGE)
-		repeated := inField.GetLabel() == descriptor.FieldDescriptorProto_LABEL_REPEATED
-		d := ""
-		switch inField.GetType() {
-		case descriptor.FieldDescriptorProto_TYPE_INT32,
-			descriptor.FieldDescriptorProto_TYPE_INT64,
-			descriptor.FieldDescriptorProto_TYPE_UINT32,
-			descriptor.FieldDescriptorProto_TYPE_UINT64,
-			descriptor.FieldDescriptorProto_TYPE_SINT32,
-			descriptor.FieldDescriptorProto_TYPE_SINT64,
-			descriptor.FieldDescriptorProto_TYPE_FIXED32,
-			descriptor.FieldDescriptorProto_TYPE_FIXED64,
-			descriptor.FieldDescriptorProto_TYPE_SFIXED32,
-			descriptor.FieldDescriptorProto_TYPE_SFIXED64:
-			d = "JE.int"
-		case descriptor.FieldDescriptorProto_TYPE_FLOAT,
-			descriptor.FieldDescriptorProto_TYPE_DOUBLE:
-			d = "JE.float"
-		case descriptor.FieldDescriptorProto_TYPE_BOOL:
-			d = "JE.bool"
-		case descriptor.FieldDescriptorProto_TYPE_STRING:
-			d = "JE.string"
-		case descriptor.FieldDescriptorProto_TYPE_ENUM:
-			// Remove leading ".".
-			d = encoderName(elmFieldType(inField))
-		case descriptor.FieldDescriptorProto_TYPE_MESSAGE:
-			// Remove leading ".".
-			d = encoderName(elmFieldType(inField))
-		case descriptor.FieldDescriptorProto_TYPE_BYTES:
-			d = "bytesFieldEncoder"
-		default:
-			return fmt.Errorf("Error generating encoder for field %s", inField.GetType())
-		}
-
-		val := argName + "." + elmFieldName(inField.GetName())
-		if repeated {
-			fg.P("%s (%q, repeatedFieldEncoder %s %s)", leading, jsonFieldName(inField), d, val)
-		} else {
-			if optional {
-				fg.P("%s (%q, optionalEncoder %s %s)", leading, jsonFieldName(inField), d, val)
-			} else {
-				fg.P("%s (%q, %s %s)", leading, jsonFieldName(inField), d, val)
-			}
-		}
-
-		leading = ","
-	}
-	fg.P("]")
-	fg.Out()
-	fg.Out()
-	return nil
 }
 
 func elmTypeName(in string) string {
