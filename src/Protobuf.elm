50874be08edb95c5b9cb922dd98ca91cd4a42e7a
Tiziano Santoro <tiziano88@gmail.com>
2017-01-01 16:48:24 +0000

    doc

--- src/Protobuf.elm
+++ src/Protobuf.elm
@@ -1,42 +1,64 @@
 module Protobuf exposing (..)
 
+{-| Runtime library for Google Protocol Buffers.
+
+# Operators
+
+@docs (<$>), (<*>)
+
+# Decoder Helpers
+
+@docs requiredFieldDecoder, optionalFieldDecoder, repeatedFieldDecoder, bytesFieldDecoder
+
+@docs withDefault
+
+# Encoder Helpers
+
+@docs requiredFieldEncoder, optionalEncoder, repeatedFieldEncoder, bytesFieldEncoder
+
+-}
+
 import Json.Decode as JD
 import Json.Encode as JE
 
 
+{-| Applicative initial application.
+-}
 (<$>) : (a -> b) -> JD.Decoder a -> JD.Decoder b
 (<$>) =
     JD.map
 
 
+{-| Applicative continued application.
+-}
 (<*>) : JD.Decoder (a -> b) -> JD.Decoder a -> JD.Decoder b
 (<*>) f v =
     f |> JD.andThen (\x -> x <$> v)
 
 
-optionalDecoder : JD.Decoder a -> JD.Decoder (Maybe a)
-optionalDecoder decoder =
-    JD.oneOf
-        [ JD.map Just decoder
-        , JD.succeed Nothing
-        ]
-
-
+{-| Decodes a required field.
+-}
 requiredFieldDecoder : String -> a -> JD.Decoder a -> JD.Decoder a
 requiredFieldDecoder name default decoder =
     withDefault default (JD.field name decoder)
 
 
+{-| Decodes an optional field.
+-}
 optionalFieldDecoder : String -> JD.Decoder a -> JD.Decoder (Maybe a)
 optionalFieldDecoder name decoder =
-    optionalDecoder (JD.field name decoder)
+    JD.maybe (JD.field name decoder)
 
 
+{-| Decodes an repeated field.
+-}
 repeatedFieldDecoder : String -> JD.Decoder a -> JD.Decoder (List a)
 repeatedFieldDecoder name decoder =
     withDefault [] (JD.field name (JD.list decoder))
 
 
+{-| Provides a default value for a field.
+-}
 withDefault : a -> JD.Decoder a -> JD.Decoder a
 withDefault default decoder =
     JD.oneOf
@@ -45,6 +67,8 @@ withDefault default decoder =
         ]
 
 
+{-| Encodes an optional field.
+-}
 optionalEncoder : String -> (a -> JE.Value) -> Maybe a -> Maybe ( String, JE.Value )
 optionalEncoder name encoder v =
     case v of
@@ -55,6 +79,8 @@ optionalEncoder name encoder v =
             Nothing
 
 
+{-| Encodes a required field.
+-}
 requiredFieldEncoder : String -> (a -> JE.Value) -> a -> a -> Maybe ( String, JE.Value )
 requiredFieldEncoder name encoder default v =
     if v == default then
@@ -63,6 +89,8 @@ requiredFieldEncoder name encoder default v =
         Just ( name, encoder v )
 
 
+{-| Encodes a repeated field.
+-}
 repeatedFieldEncoder : String -> (a -> JE.Value) -> List a -> Maybe ( String, JE.Value )
 repeatedFieldEncoder name encoder v =
     case v of
@@ -73,11 +101,15 @@ repeatedFieldEncoder name encoder v =
             Just ( name, JE.list <| List.map encoder v )
 
 
+{-| Decodes a bytes field.
+-}
 bytesFieldDecoder : JD.Decoder (List Int)
 bytesFieldDecoder =
     JD.succeed []
 
 
+{-| Encodes a bytes field.
+-}
 bytesFieldEncoder : List Int -> JE.Value
 bytesFieldEncoder v =
     JE.list []
